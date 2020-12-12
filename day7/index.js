db2da7a17ae2314b73b9fda3223396f1447c3f06
Nathan Hill <nathan99md@gmail.com>
2020-12-12 12:14:27 -0600

    Added day 7 part 2.

--- day7/index.js
+++ day7/index.js
@@ -6,7 +6,7 @@ async function run() {
     const inputString = await fs.readFileAsync('input.txt', 'utf8');
     const lines = inputString.split('\n');
 
-    let ruleParse = /(.*)\sbags?\scontain\s(\d+.*[,]?)+\./g;
+    let ruleParse = /(.*)\sbags?\scontain\s(.*[,]?)+\./g;
     let childParse = /(\d+)\s(.*)\sbags?/g;
     
     const rules = {};
@@ -15,16 +15,16 @@ async function run() {
     for(let i = 0; i < lines.length; i++) {
         currentRule = {};
         const matches = [...lines[i].matchAll(ruleParse)];
-        for(let i = 0; i < matches.length; i++) {
-            const match = matches[i];
-            currentRule.ParentBag = match[1];
-            if (!bagNames.includes(currentRule.ParentBag)) {
-                bagNames.push(currentRule.ParentBag);
-            }
-            currentRule.PossibleChildren = [];
-            let children = match[2].split(', ');
-            for(let j = 0; j < children.length; j++) {
-                let childMatches = [...children[j].matchAll(childParse)];
+        const match = matches[0];
+        currentRule.ParentBag = match[1];
+        if (!bagNames.includes(currentRule.ParentBag)) {
+            bagNames.push(currentRule.ParentBag);
+        }
+        currentRule.PossibleChildren = [];
+        let children = match[2].split(', ');
+        for(let k = 0; k < children.length; k++) {
+            if (children[k] !== 'no other bags') {
+                let childMatches = [...children[k].matchAll(childParse)];
                 let child = {
                     Quantity: Number.parseInt(childMatches[0][1]),
                     Name: childMatches[0][2]
@@ -34,12 +34,12 @@ async function run() {
                 }
                 currentRule.PossibleChildren.push(child);
             }
-            rules[currentRule.ParentBag] = currentRule;
         }
+        rules[currentRule.ParentBag] = currentRule;
     }
 
     part1(bagNames, rules);
-    // part2(groupsAnswers);
+    part2(rules);
 } 
 
 function part1(bagNames, rules) {
@@ -56,9 +56,9 @@ function part1(bagNames, rules) {
     console.log('Value needed by website is [' + sum + '].');
 }
 
-function part2(groupsAnswers) {
+function part2(rules) {
     console.log('Processing Part 2...');
-
+    let sum = getTotalChildBags('shiny gold', rules);
     console.log('Value needed by website is [' + sum + "].");
 }
 
@@ -76,6 +76,17 @@ function parentBagCanContainChildBag(parentBagName, childBagName, rules) {
         }
         return false;
     }
+}
+
+function getTotalChildBags(bagName, rules) {
+    let rule = rules[bagName];
+    let sum = 0;
+    for(let i = 0; i < rule.PossibleChildren.length; i++) {
+        let childBag = rule.PossibleChildren[i];
+        let countChildBags = getTotalChildBags(childBag.Name, rules);
+        sum += countChildBags * childBag.Quantity + childBag.Quantity;
+    }
+    return sum;
 }
 
 run();

05aa31a2137b0fbed25c27d99e13878579b89982
Nathan Hill <nathan99md@gmail.com>
2020-12-12 18:54:32 -0600

    added day 8 part 2.

--- day8/index.js
+++ day8/index.js
@@ -20,7 +20,7 @@ async function run() {
     }
     
     part1(instructions);
-    // part2(instructions);
+    part2(instructions);
 } 
 
 function part1(instructions) {
@@ -59,10 +59,61 @@ function part1(instructions) {
 
 function part2(instructions) {
     console.log('Processing Part 2...');
-    let sum = 0;
+    let retVal = 0;
 
+    let possibleOperatorsToFlip = [];
+    for(let i = 0; i < instructions.length; i++) {
+        let instruction = instructions[i];
+        if (instruction.operation === 'nop' || instruction.operation === 'jmp') {
+            possibleOperatorsToFlip.push(i);
+        }
+    }
+    
+    for (let x = 0; x < possibleOperatorsToFlip.length; x++) {
+        let previousOp = instructions[possibleOperatorsToFlip[x]].operation;
 
-    console.log('Value needed by website is [' + sum + "].");
+        if (previousOp === 'jmp') {
+            instructions[possibleOperatorsToFlip[x]].operation = 'nop';
+        } else if (previousOp === 'nop') {
+            instructions[possibleOperatorsToFlip[x]].operation = 'jmp';
+        }
+
+        let found = false;
+        let oppPoint = 0;
+        let acc = 0;
+        let executedInstructions = [];
+
+        while(!found && oppPoint < instructions.length) {
+            let currentInstruction = instructions[oppPoint];
+            if (!executedInstructions.includes(oppPoint)) {
+                let incrementPointVal = 0;
+                switch(currentInstruction.operation) {
+                    case 'nop':
+                        incrementPointVal = 1;
+                        break;
+                    case 'acc':
+                        acc += currentInstruction.value;
+                        incrementPointVal = 1;
+                        break;
+                    case 'jmp':
+                        incrementPointVal = currentInstruction.value;
+                        break;
+                }
+                executedInstructions.push(oppPoint);
+                oppPoint += incrementPointVal;
+            } else {
+                found = true;
+            }
+        }
+        if (found) {
+            instructions[possibleOperatorsToFlip[x]].operation = previousOp; 
+        } else if (oppPoint === instructions.length) {
+            retVal = acc;
+            break;
+        }
+    }
+
+    console.log('Value needed by website is [' + retVal + "].");
 }
 
 run();
